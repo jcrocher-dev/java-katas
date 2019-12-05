03868084e3e0db1f79dcbfa78e30b70589521fd2
Jean-christophe Rocher <jcrocher@orange.fr>
2019-12-05 14:10:38 +0100

    commit

--- src/kata/CaesarCipher.java
+++ src/kata/CaesarCipher.java
@@ -1,4 +1,29 @@
+
 public class CaesarCipher {
 
-    // TODO
+
+	public static void main (String[] args) {
+		
+		
+		
+		System.out.println(crypto("zabcd", -3));
+		
+	
+			
+	}
+	
+	
+	public static String crypto (String sentence, int key) {
+		
+		String alphabet= "abcdefghijklmnopqrstuvwxyz";
+		String crypted = "";
+		for (char letter : sentence.toCharArray()) {
+			int index = Math.floorMod(alphabet.indexOf(letter) + key, 26);
+			char result = alphabet.charAt(index);
+			crypted += result;
+		}
+		return crypted;
+		
+		
+	}
 }
